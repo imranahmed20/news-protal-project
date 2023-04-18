31629b45fa73101b1707e9d70cdd234830902d04
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-04-18 09:49:13 +0600

    right side added

--- src/Component/Pags/Share/RightNav/RightNav.jsx
+++ src/Component/Pags/Share/RightNav/RightNav.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import Qzone from '../QZone/Qzone';
+import bg from '../../../../assets/bg.png'
 const RightNav = () => {
     return (
         <div>
-            <h3>This is right side nav</h3>
+            <h3>Login With</h3>
+            <Button className='mb-2' variant="outline-primary"><FaGoogle /> Login with Google</Button>
+            <Button variant="outline-secondary"><FaGithub /> Login with Github</Button>
+            <div>
+                <h4 className='mt-5'>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <Qzone></Qzone>
+            <div className='text-center'>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
